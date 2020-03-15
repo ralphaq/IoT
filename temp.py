5ca059bf66edb67ba98ecd844a7e9faef35480a0
ralphaq <48308518+ralphaq@users.noreply.github.com>
2020-03-15 14:24:02 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+pin = [2, 3, 20, 21]
+btn = {"s1" : 2 , "s2" : 3, "s3": 20, "s4" : 21]
+import Adafruit_IO, time
+import RPi.GPIO as G
+
+G.setmode(G.BCM)
+aio = Adafruit_IO.Client("suryaprakash", "aio_Ernv77exF5vUONyV1qOgTTp7L1e0")
+
+for i in pin:
+    G.setup(i, G.OUT)
+    G.output(i, 0)
+
+while 1:
+    for x in btn :
+        if aio.receive(x) == "ON"
+            G.output(btn[x], 1)
+            print(btn[x], "on")
+        else :
+            G.output(btn[x], 0)
+            print(btn[x], "off")
